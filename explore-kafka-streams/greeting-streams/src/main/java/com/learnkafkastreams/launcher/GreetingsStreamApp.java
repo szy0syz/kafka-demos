adae49fef2812bd3c078ba7c155d81f2973bc541
Zhenyu Shi <zhenyu.shi@compass.com>
2023-07-10 14:20:50 +0800

    Test Greeting App

--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
@@ -21,8 +21,8 @@ public class GreetingsStreamApp {
     properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
     properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
 
-    createTopics(properties,
-        List.of(GreetingsTopology.GREETINGS, GreetingsTopology.GREETINGS_UPPERCASE));
+//    createTopics(properties,
+//        List.of(GreetingsTopology.GREETINGS, GreetingsTopology.GREETINGS_UPPERCASE));
 
     var greetingsTopology = GreetingsTopology.buildTopology();
 
