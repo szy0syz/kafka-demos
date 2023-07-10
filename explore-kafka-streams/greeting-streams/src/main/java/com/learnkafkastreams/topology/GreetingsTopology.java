8b1969f718c9b4e3f5b9317cb3f9c84a0f206dda
Zhenyu Shi <zhenyu.shi@compass.com>
2023-07-10 13:50:27 +0800

    Update Kafka Streams Launcher Application

--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
@@ -4,6 +4,7 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
 
 public class GreetingsTopology {
@@ -18,8 +19,14 @@ public class GreetingsTopology {
         .stream(GREETINGS,
             Consumed.with(Serdes.String(), Serdes.String()));
 
+    greetingsStream
+        .print(Printed.<String, String>toSysOut().withLabel("greetingsStream"));
+
     var modifiedStream =greetingsStream
         .mapValues(((readOnlyKey, value) -> value.toUpperCase()));
+
+    modifiedStream
+        .print(Printed.<String, String>toSysOut().withLabel("modifiedStream"));
 
     modifiedStream
         .to(GREETINGS_UPPERCASE,
